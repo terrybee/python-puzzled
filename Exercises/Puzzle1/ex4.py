ead8d71fefc6345e435f0d7b21040953427dcbad
tighb <yeontae@gmail.com>
2020-09-16 23:07:59 +0900

    refactoring code

--- Exercises/Puzzle1/ex4.py
+++ Exercises/Puzzle1/ex4.py
@@ -1,10 +1,11 @@
-#Programming for the Puzzled -- Srini Devadas
-#You Will All Conform
-#Input is a vector of F's and B's, in terms of forwards and backwards caps
-#Output is a set of commands (printed out) to get either all F's or all B's
-#Fewest commands are the goal
+# Programming for the Puzzled -- Srini Devadas
+# You Will All Conform
+# Input is a vector of F's and B's, in terms of forwards and backwards caps
+# Output is a set of commands (printed out) to get either all F's or all B's
+# Fewest commands are the goal
 
 input = 'BWWWWWBWWWW'
+
 
 def encode(input):
     # add end-char
@@ -13,33 +14,35 @@ def encode(input):
 
     start = 0
     for i in range(1, len(input)):
-        if (input[i-1] == input[i]):
+        if input[i - 1] == input[i]:
             continue
         result.append(str(i - start) + input[start])
         start = i
     return ''.join(result)
+
 
 def decode(input):
     result = []
     start = 0
     alphaCount = 0
     result = []
-    
+
     for i in range(1, len(input)):
         # Numeric
-        if input[i].isalpha() == False:
+        if not input[i].isalpha():
             continue
         alphaCount = int(input[start:i])
-        start = i+1
+        start = i + 1
         for n in range(alphaCount):
             result.append(input[i])
-    
+
     return ''.join(result)
+
 
 # Run
 encodedStr = encode(input)
 print(encodedStr)
 decodedStr = decode(encodedStr)
 if decodedStr != input:
-    SystemError('Error: %s != %s'  (input, decodedStr))
+    SystemError('Error: %s != %s'(input, decodedStr))
 print(decodedStr)
--- Exercises/Puzzle1/line.py
+++ Exercises/Puzzle1/line.py
@@ -1,11 +1,12 @@
-#Programming for the Puzzled -- Srini Devadas
-#You Will All Conform
-#Input is a vector of F's and B's, in terms of forwards and backwards caps
-#Output is a set of commands (printed out) to get either all F's or all B's
-#Fewest commands are the goal
+# Programming for the Puzzled -- Srini Devadas
+# You Will All Conform
+# Input is a vector of F's and B's, in terms of forwards and backwards caps
+# Output is a set of commands (printed out) to get either all F's or all B's
+# Fewest commands are the goal
 
-caps = ['F', 'F', 'B', 'B', 'B', 'F', 'B', 'B', 'B', 'F', 'F', 'B', 'F' ]
-cap2 = ['F', 'F', 'B', 'B', 'B', 'F', 'B', 'B', 'B', 'F', 'F', 'F', 'F' ]
+caps = ['F', 'F', 'B', 'B', 'B', 'F', 'B', 'B', 'B', 'F', 'F', 'B', 'F']
+cap2 = ['F', 'F', 'B', 'B', 'B', 'F', 'B', 'B', 'B', 'F', 'F', 'F', 'F']
+
 
 def pleaseConform(c):
     # init var
@@ -15,13 +16,13 @@ def pleaseConform(c):
 
     for i in range(1, len(caps)):
         # if caps are the same
-        if(c[start] == c[i]):
+        if c[start] == c[i]:
             continue
-        
+
         # else caps are different
         if isFlipSection:
             # command flip
-            print("flip: from", start , "to", i-1)
+            print("flip: from", start, "to", i - 1)
 
         # change isFlipSection, start
         start = i
